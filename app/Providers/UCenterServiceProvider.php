4e01699a7b6db30e3e1a7d1e7cb80373442c2931
彭泽宽 <pengzekuan@gmail.com>
2020-11-26 21:25:31 +0800

    feat: 迁移文件路径修复

--- app/Providers/UCenterServiceProvider.php
+++ app/Providers/UCenterServiceProvider.php
@@ -10,16 +10,16 @@ class UCenterServiceProvider extends ServiceProvider
         // 配置文件迁移
         if (function_exists('config_path')) {
             $this->publishes([
-                __DIR__ . '/../config/ucenter.php' => config_path('ucenter.php')
+                __DIR__ . '/../../config/ucenter.php' => config_path('ucenter.php')
             ], 'config');
 
             // 数据库迁移脚本
-            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+            $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
         }
 
 
         // 控制器，路由迁移
-        $this->loadRoutesFrom(__DIR__ . '/../routes/ucenter.php');
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/ucenter.php');
 
         if ($this->app->runningInConsole()) {
             $this->commands([]);
@@ -34,7 +34,7 @@ class UCenterServiceProvider extends ServiceProvider
 
         // 合并配置
         $this->mergeConfigFrom(
-            __DIR__ . '/../config/ucenter.php',
+            __DIR__ . '/../../config/ucenter.php',
             'ucenter'
         );
     }
